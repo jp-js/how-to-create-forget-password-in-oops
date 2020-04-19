a542f5cd137aad9b8dc9855295581fb93427a339
Jagseer singh <52886945+jp-js@users.noreply.github.com>
2020-04-19 15:54:04 +0530

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php require("db.php");
-if(isset($_GET['id'])){
-$id = $_GET['id'];
-mysqli_query($con,"DELETE FROM comments WHERE id='$id'");
-$message = "Comment Delete Successfully..";
-header("location: index.php?message=".$message."");
-}
-mysqli_close($con);
-?>
